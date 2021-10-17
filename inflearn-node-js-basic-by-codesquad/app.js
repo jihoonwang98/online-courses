967b741823c8cdfa6d1e0aace6ca2aba6782d5ae
왕지훈 <jihoonwang98@naver.com>
2021-10-17 20:44:04 +0900

    git ignore 추가

--- inflearn-node-js-basic-by-codesquad/app.js
+++ inflearn-node-js-basic-by-codesquad/app.js
@@ -8,14 +8,23 @@ app.listen(3000, function() {
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');  // 'view engine은 ejs야'라고 말해줌 
 
+
+// url routing
 app.get('/', function(req, res) {
   // req.param('email')   ->  email이라는 이름의 parameter value
   res.sendFile(__dirname + '/public/main.html');
 });
 
-app.post('/email_post', function(req, res) {
-  console.log(req.body); // { email: 'jihoonwang98@naver.com' }     
-  console.log(req.body.email); // jihoonwang98@naver.com
-  res.send('<h1>welcome!! ' + req.body.email + '</h1>');
+app.post('/email_post', function(req, res) { 
+  console.log(req.body.email);
+  res.render('email.ejs', {'email': req.body.email}); 
 });
+
+app.post('/ajax_send_mail', function(req, res) {
+  console.log(req.body.email); 
+  var responseData = {'result': 'ok', 'email': req.body.email};
+  res.json(responseData); 
+});
+
