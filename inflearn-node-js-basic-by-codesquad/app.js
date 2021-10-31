715a622cf3859ede8e85c6e62eda91e2d7b49391
왕지훈 <jihoonwang98@naver.com>
2021-10-31 13:57:09 +0900

    Node.js 3장 정리중

--- inflearn-node-js-basic-by-codesquad/app.js
+++ inflearn-node-js-basic-by-codesquad/app.js
@@ -1,30 +1,56 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.listen(3000, function() {
-	console.log('start! express server on port 3000');
+var mysql = require('mysql');
+
+var dbConnection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'mojo',
+    password: 'mojo',
+    database: 'practice',
+});
+dbConnection.connect();
+
+app.listen(3000, function () {
+    console.log('start! express server on port 3000');
 });
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');  // 'view engine은 ejs야'라고 말해줌 
 
 
 // url routing
-app.get('/', function(req, res) {
-  // req.param('email')   ->  email이라는 이름의 parameter value
-  res.sendFile(__dirname + '/public/main.html');
+app.get('/', function (req, res) {
+    // req.param('email')   ->  email이라는 이름의 parameter value
+    res.sendFile(__dirname + '/public/main.html');
 });
 
-app.post('/email_post', function(req, res) { 
-  console.log(req.body.email);
-  res.render('email.ejs', {'email': req.body.email}); 
+app.post('/email_post', function (req, res) {
+    console.log(req.body.email);
+    res.render('email.ejs', {'email': req.body.email});
 });
 
-app.post('/ajax_send_mail', function(req, res) {
-  console.log(req.body.email); 
-  var responseData = {'result': 'ok', 'email': req.body.email};
-  res.json(responseData); 
+app.post('/ajax_send_mail', function (req, res) {
+    console.log('email: ', req.body.email);
+    var email = req.body.email;
+    var responseData = {};
+    dbConnection.query(`select name
+                        from user
+                        where email = '${email}'`, function (err, rows) {
+        if (err) throw err;
+        if (rows[0]) {
+            console.log('rows[0]: ', rows[0]);
+            responseData.result = "ok";
+            responseData.name = rows[0].name;
+        } else {
+            responseData.result = 'none';
+            responseData.name = '';
+        }
+
+        res.json(responseData);
+    });
 });
 
---
+++ inflearn-node-js-basic-by-codesquad/정리/[Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해] 섹션 3. Database 연동 기본.md
@@ -0,0 +1,33 @@
+# [Node.js 웹개발로 알아보는 백엔드 자바스크립트의 이해] 섹션 3. Database 연동 기본
+
+> [인프런](https://www.inflearn.com/course/node-js-%EC%9B%B9%EA%B0%9C%EB%B0%9C)
+>
+> [실습 코드 - 깃허브](https://github.com/crongro/node_server_inflearn)
+
+
+
+### 목차
+
+- MySQL 연동 설정
+- MySQL 연동 구현
+
+
+
+
+
+## \#1 MySQL 연동 설정
+
+
+
+
+
+
+
+## \#2 MySQL 연동 구현
+
+
+
+
+
+
+
